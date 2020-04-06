70884936ce68ebfcb7f5f730907ca3eb75f51df2
dsuico <dale.alexis96@gmail.com>
2020-04-06 17:15:24 +0800

    add database folder

---
+++ database/migrations/2018_12_08_132905_create_schedules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('name',100);
+            $table->text('description');
+            $table->boolean('is_completed')->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete(null)->onUpdate(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
---
+++ database/migrations/2018_12_11_111401_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',100);
+            $table->integer('schedule_id')->unsigned();
+            $table->boolean('is_completed')->default(0);
+            $table->timestamps();
+            $table->foreign('schedule_id')->references('id')->on('schedules')
+                ->onDelete(null)->onUpdate(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
